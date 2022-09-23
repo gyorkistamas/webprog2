dc57e738de903b2b50f874109aa40610e0cd2af6
Tamás Györkis <tamasgyorkis@gmail.com>
2022-09-23 12:56:08 +0200

    Update profil.php
    typo

--- profil.php
+++ profil.php
@@ -11,7 +11,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
-    <title>Profil Módosítása</title>
+    <title>Profil Módosítás</title>
 </head>
 
 <body>
